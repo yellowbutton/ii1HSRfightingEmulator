375835acd6b8db4224fbd71b7581af3a65f5429b
yellowbutton <zz_3_1415926@163.com>
2026-04-16 19:11:05 +0800

    例行上传

--- 工具库.py
+++ 工具库.py
@@ -1,4 +1,7 @@
-def 选取(身份,字典列表):
+def 选取(值,字典列表,标签="身份",列表返回=False):
+    if 列表返回:
+        return [字典 for 字典 in 字典列表 if 标签 in 字典 and 字典[标签]==值]
     for 字典 in 字典列表:
-        if 字典["身份"]==身份:
+        if 标签 in 字典 and 字典[标签]==值:
             return 字典
+    return None
